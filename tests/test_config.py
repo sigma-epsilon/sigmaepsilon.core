cb9840cbb4d9c86e450c4afae87e8a3e8ca8a54b
Bence Balogh <benceeok@gmail.com>
2024-02-19 12:34:05 +0100

    fixed tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -17,15 +17,14 @@ class TestConfig(SigmaEpsilonTestCase):
         pyproject_toml_path = find_pyproject_toml(start_dir, max_depth=0)
         config = load_pyproject_config(filepath=pyproject_toml_path)
         self.assertTrue(isinstance(config, dict))
-        config = load_pyproject_config(filepath=pyproject_toml_path, section="tool.poetry")
+        config = load_pyproject_config(filepath=pyproject_toml_path)
         self.assertTrue(isinstance(config, dict))
         config = load_pyproject_config(
-            filepath=pyproject_toml_path, section=["tool.poetry", "build-system"]
+            filepath=pyproject_toml_path, section=["build-system"]
         )
         self.assertTrue(isinstance(config, list))
-        self.assertTrue(len(config) == 2)
+        self.assertTrue(len(config) == 1)
         self.assertTrue(isinstance(config[0], dict))
-        self.assertTrue(isinstance(config[1], dict))
         self.assertFailsProperly(
             TypeError, find_pyproject_toml, start_dir, max_depth="0"
         )
@@ -34,10 +33,9 @@ class TestConfig(SigmaEpsilonTestCase):
         )
         self.assertFailsProperly(TypeError, find_pyproject_toml, 1, max_depth=-1)
         
-        config = load_pyproject_config(filepath=pyproject_toml_path, section="tool.poetry")
+        config = load_pyproject_config(filepath=pyproject_toml_path)
         package_name = namespace_package_name(dirname(abspath(sc.__file__)), 10)
-        print(config, pyproject_toml_path, package_name)
-        #self.assertEqual(package_name, config["name"])
+        self.assertEqual(package_name, config["name"])
         
         self.assertFailsProperly(
             TypeError, namespace_package_name, start_dir, max_depth="0"
