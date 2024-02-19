824d94067dad2ff9fe8601a937795fdf58425a0d
Bence Balogh <benceeok@gmail.com>
2024-02-19 12:40:24 +0100

    updated

--- poetry_export.sh
+++ poetry_export.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-ECHO "Exporting poetry dependencies to requirements.txt"
 poetry export -f requirements.txt --output requirements.txt
-ECHO "Exporting poetry dependencies to requirements-test.txt"
 poetry export -f requirements.txt --only test --output requirements-test.txt
-ECHO "Exporting poetry dependencies to requirements-dev.txt"
 poetry export -f requirements.txt --only dev --output requirements-dev.txt
-ECHO "Exporting poetry dependencies to docs/requirements.txt"
 poetry export -f requirements.txt --only docs --output ./docs/requirements.txt
-ECHO "All done!"
